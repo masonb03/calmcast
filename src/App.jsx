c459d5cc23cda40330823f718400c5c57b1fa992
Mason <bevinsm355@gmail.com>
2025-11-20 13:39:33 -0500

    feat: add Login and Signup components; implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import SelectedWeather from "./components/SelectedWeather";
 import WeatherList from "./components/WeatherList";
 import Nav from "./features/Nav";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
@@ -13,36 +16,43 @@ function App() {
 
   return (
     <div className="app-container">
-      <input 
-      type="text"
-      placeholder="Search for your city..."
-      value={location}
-      onChange={(e) => setLocation(e.target.value)}
-      />
-      <button onClick={()=>{
-        setSelectedWeather(location);
-        setForecast(false);
-      }}>Check Current Weather</button>
-
-      <button onClick={() =>{
-        setSelectedWeather(location);
-        setForecast(true);
-      }}>Check Something Else!</button>
-
-      {selectedWeather && !forecast && (
-        <SelectedWeather
-        location={selectedWeather}
-        setSelectedWeather={setSelectedWeather}
-        />
-      )}
-        {selectedWeather && forecast && (
-          <WeatherList
-          location={selectedWeather}
-          setSelectedWeather={setSelectedWeather}
-          />
-       
-      )}
-
+      <div className="nav-bar">
+        <Nav />
+      </div>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+      <div className=''>
+          <input 
+            type="text"
+            placeholder="Search for your city..."
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            />
+          <button onClick={()=>{
+              setSelectedWeather(location);
+              setForecast(false);
+            }}>Check Current Weather
+          </button>
+          <button onClick={() =>{
+              setSelectedWeather(location);
+              setForecast(true);
+            }}>Check Something Else!
+          </button>
+          {selectedWeather && !forecast && (
+            <SelectedWeather
+            location={selectedWeather}
+            setSelectedWeather={setSelectedWeather}
+            />
+          )}
+            {selectedWeather && forecast && (
+              <WeatherList
+              location={selectedWeather}
+              setSelectedWeather={setSelectedWeather}
+              />
+          )}
+      </div>
     </div>
   )
 }
